58f3ecf822a23a62961d321fcf88ddfc458ab28c
Mwadali <wellingtonmwadali@gmail.com>
2023-06-25 12:46:41 -0700

    header

--- sort.h
+++ sort.h
@@ -6,16 +6,31 @@
 
 #define UP 0
 #define DOWN 1
-
-typedef enum bool {
-    false = 0,
-    true
+/**
+ * enum bool - enumeration boolen to return
+ * true or false values
+ * False: 0
+ * True: 1(automatically assumes 1)
+ */
+typedef enum bool
+{
+	false = 0,
+	true
 } bool;
 
-typedef struct listint_s {
-    int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
