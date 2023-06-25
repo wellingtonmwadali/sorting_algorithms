1ddd3cc8924e87aee85cea000ec87488b2b5c31c
Alex Maina <lexizgichbuoy@gmail.com>
2023-06-25 13:49:43 -0700

    update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,23 +4,23 @@
  * swap_nodes - function that swap two nodes in a listint_t
  * doubly-linked list.
  *
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
+ * @head: pointer to the head of the doubly-linked list.
+ * @node1: first node to swap.
+ * @node2: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
 {
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
 	else
-		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
